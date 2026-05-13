d7095b52d091ea74e324fbdfc7522dafdc29a477
Varun Verma <vermava1@msu.edu>
2026-05-13 13:40:53 +0530

    MacWindow setup & Author's note
    Added:
    /platform/mac/MacWindow.h - Added a virtual destructor to the MacWindow class
    Added 'init', 'update' and 'close' all as overrides to match the base class
    Added a tiny note in main.cpp in the hopes somebody may come across it
    
    Changes:
    
    Moved 'class MacWindow' from platform.h to MacWindow.h to keep platform.h as a pure interface.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 //
 // Created by Varun Verma on 5/13/26.
+// Hello to whoever reads this! This is Varun here! I'm so excited to finally get started
+// on creating Vuron, a project I have been thinking about for a long time. I hope this
+// project is useful to anybody and everybody. I think great software should be free, so
+// if this isn't great, or isn't free, then my job isn't done. Vuron is going to be an
+// ultra-fast Game engine, capable of anything you want it to do, and exactly that. Nothing
+// more, nothing less. I hope this may come in handy to somebody some day. To anybody who
+// bothered reading this. I thank you, and I hope you can appreciate this project once it
+// is completed.
 //
 
--- src/platform/mac/MacWindow.h
+++ src/platform/mac/MacWindow.h
@@ -5,6 +5,31 @@
 #ifndef VURON_MACWINDOW_H
 #define VURON_MACWINDOW_H
 
+#include "../platform.h"
 
+namespace vuron
+{
+    class MacWindow : public PlatformWindow
+    {
+    public:
+        MacWindow() : m_windowHandle(nullptr) {} // Constructor
+        // the part after the ':' is the Initializer list
+        // This sets the window pointer (nullptr) to 0 as soon as it's created
+        // Avoids garbage data and Undefined Behavior (UB)
+        virtual ~MacWindow() { close(); } // Destructor
+        // Now, whenever a MacWindow goes out of scope, it calls close()
+        // It's a safety net to make sure the window is destroyed if we forget
+
+        // Used 'override' keyword to make sure these match
+        // the base class of PlatformWindow in platform.h
+        bool init() override;
+        void update() override;
+        void close() override;
+
+    private:
+        void* m_windowHandle; // Using void* because we don't want
+        // Mac-specific headers in the C++ headers YET
+    };
+}
 
 #endif //VURON_MACWINDOW_H
